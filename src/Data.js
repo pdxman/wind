50eb60344ee6df206b7461ac1ecccebdd739bd78
Crawford Vining <crawpdx@gmail.com>
2021-08-28 13:45:23 -0700

    added conditionals; began data visualization; updated name of location

--- src/Data.js
+++ src/Data.js
@@ -7,28 +7,32 @@ export default function Data() {
     const [weatherData, setWeatherData] = useState([])  
     const [loc, setLoc] = useState('new york')
     const [direction, setDirection] = useState('')
+    const [name, setname] = useState('new york')
     
     useEffect(() => {
        getLoc()
-       
+    //    console.log("loc inside useEffect: ", loc)
     }, [])
 
     const getLoc = () => {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${key}`)
         .then(response => {
-            console.log(response.data.wind)
+            // console.log(response.data.wind)
             setWeatherData(response.data.wind)
+            setname(response.data.name)
+            // console.log("the name: ", {name})
         })
     }
 
     const runDegreeConversion = () => {
         var conversionResult = degToCompass(weatherData.deg);
-        console.log("wind direction ", conversionResult)
+        //console.log("wind direction ", conversionResult)
         setDirection(conversionResult)
     }
 
     const handleSearch = event => {
         event.preventDefault();
+        event.persist();
         getLoc()
         runDegreeConversion()
         //fires get loc on form submit- undefined
@@ -49,9 +53,22 @@ export default function Data() {
         return arr[ Math.abs(val) ] ;
     }
 
+    const windSpeed = false
+    const windGust = weatherData.deg
+
+    // const updateDirection = () => {
+    //     console.log('updated direction')
+    // }
+
+    if(weatherData.deg < 180) {
+        console.log(weatherData.deg, 'less than 180')
+    } else {
+        console.log(weatherData.deg, 'more than 180')
+    }
+
     return(
         <div>
-              <p>Wind Data for {loc}</p>
+              <p>Wind Data for {name}</p>
               <form onSubmit={handleSearch}>
                 <input 
                     type="text"
@@ -59,12 +76,36 @@ export default function Data() {
                 />
                 <button type="submit">Search</button> 
               </form>  
-              <p><strong>Current Search Location:</strong> {loc}</p>
+              <p><strong>Current Search Location:</strong> {name}</p>
               <p><strong>Wind Speed:</strong> {weatherData.speed}</p>   
+              <p><strong>Wind Direction(degrees):</strong> {weatherData.deg}</p> 
               <p><strong>Wind Gust:</strong> {weatherData.gust}</p>   
-              <p><strong>Wind Direction(degrees):</strong> {weatherData.deg}</p>   
               {/* <button onClick={runDegreeConversion}>Convert to Wind Direction</button> */}
               <p><strong>Wind Direction(Compass):</strong> {direction}</p>
+              <div style={{
+                  width:  '4em',
+                  height: '4em', 
+                  background: 'green',
+                  borderRadius: '50%',
+                  margin: '5em auto',
+                  position: 'relative'
+              }}>
+                  <div
+                    className="Rotate-circle"
+                    style={{
+                        animation: 'app-logo-spin infinite spin ease',
+                        width:  '1em',
+                        height: '1em', 
+                        background: 'red',
+                        borderRadius: '50%',
+                        position: 'absolute',
+                        top: weatherData.deg < 180 ? '50px' : '10px',
+                        left: '5px'
+                        
+                        // transform: windSpeed ? 'rotate(0deg) translateX(36px) rotate(0deg)' : 'rotate(-45deg)' 
+                    }}>
+                </div>
+              </div>
         </div>
     )
 }
