08ba198bff5edfaf03fb17830d7e030ee72f8ec0
Crawford Vining <crawpdx@gmail.com>
2021-09-03 08:42:16 -0700

    cleanup of styles, comments and unused code

--- src/Data.js
+++ src/Data.js
@@ -46,52 +46,36 @@ export default function Data() {
         return arr[ Math.abs(val) ] ;
     }
 
-    const windSpeed = false
-    const windGust = weatherData.deg
-
-    if(weatherData.deg < 180) {
-        console.log(weatherData.deg, 'less than 180')
-    } else {
-        console.log(weatherData.deg, 'more than 180')
-    }
-
+    const circleStyle = {
+        transition: 'all 1s ease',
+        width:  '1em',
+        height: '1em', 
+        background: 'grey',
+        borderRadius: '50%',
+        position: 'absolute',
+        top: weatherData.deg < 180 ? '50px' : '10px',
+        left: '5px' }
+       
     return(
         <div>
-              <p>Wind Data for {name}</p>
-              <form onSubmit={handleSearch}>
+            <p>Wind Data for {name}</p>
+            <form onSubmit={handleSearch}>
                 <input 
                     type="text"
                     onChange={updateSearch}
                     placeholder="Search for Location"
                 />
-                <button type="submit">Search</button> 
-              </form>  
+                <button type="submit">SEARCH</button> 
+            </form> 
               <p><strong>Wind Speed:</strong> {weatherData.speed}</p>   
               <p><strong>Wind Direction(degrees):</strong> {weatherData.deg}</p> 
               <p><strong>Wind Gust:</strong> {weatherData.gust}</p>   
-              {/* <button onClick={runDegreeConversion}>Convert to Wind Direction</button> */}
               <p><strong>Wind Direction(Compass):</strong> {direction}</p>
-              <div style={{
-                  width:  '4em',
-                  height: '4em', 
-                  background: '#003366',
-                  borderRadius: '50%',
-                  margin: '5em auto',
-                  position: 'relative'
-              }}>
+              <div className="circle-large">
                   <div
-                    className="Rotate-circle"
-                    style={{
-                        // animation: 'app-logo-spin infinite spin ease',
-                        transition: 'all 1s ease',
-                        width:  '1em',
-                        height: '1em', 
-                        background: 'grey',
-                        borderRadius: '50%',
-                        position: 'absolute',
-                        top: weatherData.deg < 180 ? '50px' : '10px',
-                        left: '5px'
-                    }}>
+                    className="Rotate-circle" 
+                    style={circleStyle}
+                  >
                 </div>
               </div>
         </div>
